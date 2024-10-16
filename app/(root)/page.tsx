87f6338a7cf153351a9602330f3d8fd51bb4543b
D_Zucho <danny.nico.dev@outlook.com>
2024-10-15 17:07:10 -0700

    initial setup with next shadcn and jsm editor

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </div>
+  )
+}
+
+export default Home

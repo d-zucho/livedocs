b78501505ae20f68344f0adbe8d0fa26c54b37e3
D_Zucho <danny.nico.dev@outlook.com>
2024-10-16 19:29:43 -0700

    connect with liveblocks

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter as FontSans } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
 import { dark } from '@clerk/themes'
+import Provider from './Provider'
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -37,7 +38,7 @@ export default function RootLayout({
             fontSans.variable
           )}
         >
-          {children}
+          <Provider>{children}</Provider>
         </body>
       </html>
     </ClerkProvider>
